7643c121a198217ea90584b12fb48af8c88be940
Viktor Potemkin <99296303+potemkinViktor@users.noreply.github.com>
2022-06-03 06:13:34 +0300

    Update NTokenSale.sol

--- contracts/NTokenSale.sol
+++ contracts/NTokenSale.sol
@@ -102,11 +102,8 @@ contract NTokenSale {
 
     /// @notice users buy locked tokens by transferring BUSD to this contract 
     /// @param amount Amount of BUSD tokens to deposit
-<<<<<<< HEAD
     function buyTokens(uint256 amount) public  onlyWhitelisted {
-=======
-    function buyTokens(uint256 amount) public onlyWhitelisted(){
->>>>>>> 2b0c7161e886e320e078e7e91ca01158b910be2e
+
         address _msgSender = msg.sender; 
 
         require(!buyers[_msgSender].bougt);
@@ -227,11 +224,7 @@ contract NTokenSale {
         }
     }
 
-<<<<<<< HEAD
     function getBalanceBUSD() public view returns(uint256){
-=======
-    function getBalanceBUSD() public returns(uint256){
->>>>>>> 2b0c7161e886e320e078e7e91ca01158b910be2e
         return busdAddress.balanceOf(address(this));
     }
 
@@ -247,8 +240,3 @@ contract NTokenSale {
         return tokenContract.balanceOf(msg.sender);
     }
 }
-
-<<<<<<< HEAD
-=======
-
->>>>>>> 2b0c7161e886e320e078e7e91ca01158b910be2e
