c6204e4685c54d2feccc9cade54a94892e5dba4b
Viktor Potemkin <99296303+potemkinViktor@users.noreply.github.com>
2022-06-06 15:13:34 +0300

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -8,12 +8,12 @@ async function main() {
     "0xcDA8eD22bB27Fe84615f368D09B5A8Afe4a99320", // tokenContract
     "0x78867BbEeF44f2326bF8DDd1941a4439382EF2A7", // busdAddress
     800000, // saleSupply
-    9999999, // timeEndSale in seconds
+    600, // timeEndSale in seconds 7 days = 604800
     "0x78867BbEeF44f2326bF8DDd1941a4439382EF2A7", // BUSD in bsc testnet
     "0xcDA8eD22bB27Fe84615f368D09B5A8Afe4a99320", // NarfexAddress in bsc testnet
     "0xf47644e079303263a2de0829895d000900d2fab8", // pair Narfex -> BUSD in PancakeSwap
-    30000, // minimum amount of deposit to buy in busd for each user
-    100000, // maximum amount of deposit for sale in busd for each user
+    1, // minimum amount of deposit to buy in busd for each user
+    3, // maximum amount of deposit for sale in busd for each user
     );
   await nTokenSale.deployed();
 
