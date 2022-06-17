6c1a1ab8494724173d633ab19e83b494e8365822
Viktor Potemkin <99296303+potemkinViktor@users.noreply.github.com>
2022-06-17 14:00:14 +0300

    Update deployFactory.js

--- scripts/deployFactory.js
+++ scripts/deployFactory.js
@@ -5,10 +5,10 @@ async function main() {
   const Factory = await ethers.getContractFactory("Factory");
 
   const factory = await Factory.deploy(
-    "0x78867BbEeF44f2326bF8DDd1941a4439382EF2A7", // busdAddress
-    "0xcDA8eD22bB27Fe84615f368D09B5A8Afe4a99320", // tokenContract
-    "0xfe72323a3dAa1a491CBaF32532De0EFBF9eB4B70", // tokenSaleContract
-    //"0x29572D9CC8c8687303D28Ab5eD7c96e1Cee2aE91", // factoryOwner
+    "0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56", // busdAddress
+    "0x3764Be118a1e09257851A3BD636D48DFeab5CAFE", // tokenContract
+    "0xFbA1906e682BF0032D26EfBA6bFC5229a663B968", // tokenSaleContract //0x9cE316e703C6BdcaD8ada3dCF2d3a4E3911C0d4c
+    "0x9e8db3942797d2578f48caf5663eb22e286ad84b", // factoryOwner
   );
   await factory.deployed();
 
@@ -17,10 +17,10 @@ async function main() {
   const Pool = await ethers.getContractFactory("Pool");
 
   const pool = await Pool.deploy(
-    "0x78867BbEeF44f2326bF8DDd1941a4439382EF2A7", // busdAddress
-    "0xcDA8eD22bB27Fe84615f368D09B5A8Afe4a99320", // tokenContract
-    "0xfe72323a3dAa1a491CBaF32532De0EFBF9eB4B70", // tokenSaleContract
-    "0x29572D9CC8c8687303D28Ab5eD7c96e1Cee2aE91", // factoryOwner
+    "0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56", // busdAddress
+    "0x3764Be118a1e09257851A3BD636D48DFeab5CAFE", // tokenContract
+    "0xFbA1906e682BF0032D26EfBA6bFC5229a663B968", // tokenSaleContract
+    "0x9e8db3942797d2578f48caf5663eb22e286ad84b", // factoryOwner
     2, // maxPoolAmount
   );
   await pool.deployed();
@@ -34,4 +34,40 @@ main()
     console.error(error);
     process.exit(1);
   });
-  // 0xcDA8eD22bB27Fe84615f368D09B5A8Afe4a99320
+
+// const hre = require("hardhat");
+
+// async function main() {
+
+//   const Factory = await ethers.getContractFactory("Factory");
+
+//   const factory = await Factory.deploy(
+//     "0x78867BbEeF44f2326bF8DDd1941a4439382EF2A7", // busdAddress
+//     "0xcDA8eD22bB27Fe84615f368D09B5A8Afe4a99320", // tokenContract
+//     "0x9cE316e703C6BdcaD8ada3dCF2d3a4E3911C0d4c", // tokenSaleContract //0x9cE316e703C6BdcaD8ada3dCF2d3a4E3911C0d4c
+//     "0x9e8db3942797d2578f48caf5663eb22e286ad84b", // factoryOwner
+//   );
+//   await factory.deployed();
+
+//   console.log("Factory deployed to:", factory.address);
+
+//   const Pool = await ethers.getContractFactory("Pool");
+
+//   const pool = await Pool.deploy(
+//     "0x78867BbEeF44f2326bF8DDd1941a4439382EF2A7", // busdAddress
+//     "0xcDA8eD22bB27Fe84615f368D09B5A8Afe4a99320", // tokenContract
+//     "0x9cE316e703C6BdcaD8ada3dCF2d3a4E3911C0d4c", // tokenSaleContract
+//     "0x9e8db3942797d2578f48caf5663eb22e286ad84b", // factoryOwner
+//     2, // maxPoolAmount
+//   );
+//   await pool.deployed();
+
+//   console.log("Pool deployed to:", pool.address);
+// }
+
+// main() 
+//   .then(() => process.exit(0))
+//   .catch((error) => {
+//     console.error(error);
+//     process.exit(1);
+//   });
