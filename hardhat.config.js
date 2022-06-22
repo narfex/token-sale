8b00771c902856dd173ab06c4b57bd12b529d839
Danil Sakhinov <ldevil2209@ya.ru>
2022-06-23 00:02:28 +0800

    Fix factory deploy scripts

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,17 +1,6 @@
 require("@nomiclabs/hardhat-waffle");
 require("@nomiclabs/hardhat-etherscan");
-
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
-
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
-
- require('dotenv').config()
- const MORALIS_URL = process.env.MORALIS_URL
- const PRIVATE_KEY = process.env.PRIVATE_KEY
+const accounts = require('../accounts');
  
 const networks = {
   bsc: {
