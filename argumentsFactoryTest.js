0250a00097ed3ecbac218ed76bb88b9287dce8e4
Danil Sakhinov <ldevil2209@ya.ru>
2022-06-28 14:18:53 +0800

    Change NarfexTokenSale testnet contract address

--- argumentsFactoryTest.js
+++ argumentsFactoryTest.js
@@ -1,7 +1,7 @@
 module.exports = [
 	"0x78867bbeef44f2326bf8ddd1941a4439382ef2a7", // busdAddress
   "0xcDA8eD22bB27Fe84615f368D09B5A8Afe4a99320", // tokenContract
-  "0x9cE316e703C6BdcaD8ada3dCF2d3a4E3911C0d4c", // tokenSaleContract
+  "0x792207721e522584E6D140546a2AE64D9f766B78", // tokenSaleContract
   "0xa4FF4DBb11F3186a1e96d3e8DD232E31159Ded9B", // factoryOwner
   Number(1 * (10**18)).toFixed(0),
   Number(5 * (10**18)).toFixed(0),
