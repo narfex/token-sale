46dd45e042e63eab014c5a679d86c984432a6e81
Danil Sakhinov <ldevil2209@ya.ru>
2022-06-28 17:30:01 +0800

    Change NarfexTokenSale address

--- argumentsPoolTest.js
+++ argumentsPoolTest.js
@@ -1,7 +1,7 @@
 module.exports = [
 	"0x78867bbeef44f2326bf8ddd1941a4439382ef2a7", // busdAddress
   "0xcDA8eD22bB27Fe84615f368D09B5A8Afe4a99320", // tokenContract
-  "0x792207721e522584E6D140546a2AE64D9f766B78", // tokenSaleContract
+  "0x33b6bFa80ed5C8f935D6F745787Cc5EAa4736b55", // tokenSaleContract
   "0xa4FF4DBb11F3186a1e96d3e8DD232E31159Ded9B", // factoryOwner
   Number(10 * (10**18)).toFixed(0), // maxPoolAmount
   Number(1 * (10**18)).toFixed(0), // minimum deposit for user in pools
